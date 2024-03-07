0c940cf52cd668c930115d34b327d84bae88956d
usmanov <azikusmanov882@gmail.com>
2024-03-07 22:13:33 +0500

    initial commit

---
+++ src/main/java/com/example/springbootsolid/SpringBootSolidApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootsolid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSolidApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootSolidApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springbootsolid/SpringBootSolidApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootsolid;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootSolidApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
